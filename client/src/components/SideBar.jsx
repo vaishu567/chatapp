ff8e7462f08cd71a89cfd7d6248e56845fe88430
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-15 15:32:14 +0530

    okay

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "../styles/style.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const SideBar = () => {
-  return <div className="sidebar-conatiner">SideBar</div>;
+  return (
+    <div className="sidebar-conatiner">
+      <div className="sb-header">
+        <AccountCircleIcon />
+      </div>
+      <div className="sb-search">search</div>
+      <div className="sb-conversation">converstions</div>
+    </div>
+  );
 };
 
 export default SideBar;
