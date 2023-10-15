868a93f857237e5601eebf0532ddbb53dd959257
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-15 16:12:52 +0530

    commit this

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import "../styles/style.css";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import SearchIcon from "@mui/icons-material/Search";
+import { IconButton } from "@mui/material";
 
 const SideBar = () => {
   return (
     <div className="sidebar-conatiner">
       <div className="sb-header">
-        <AccountCircleIcon />
+        <div>
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
+        </div>
+        <div>
+          <IconButton>
+            <PersonAddIcon />
+          </IconButton>
+          <IconButton>
+            <GroupAddIcon />
+          </IconButton>
+          <IconButton>
+            <AddCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LightModeIcon />
+          </IconButton>
+        </div>
       </div>
-      <div className="sb-search">search</div>
+      <div className="sb-search">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input placeholder="search" className="search-box" />
+      </div>
       <div className="sb-conversation">converstions</div>
     </div>
   );
